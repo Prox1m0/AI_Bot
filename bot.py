e69dcf461b097678953bf9eeadd31ee79fe537bf
tarazz <tarasztc@gmail.com>
2025-07-10 12:43:58 +0300

    Added 2nd AI Model (Mistral Mini)

--- bot.py
+++ bot.py
@@ -7,15 +7,23 @@ def main():
     
     my_bot = Application.builder().token(settings.API_BOT).build()
     
-    my_bot.add_handler(CommandHandler("start", start))
     chat_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), ask)
+    mistral_handler = MessageHandler(filters.TEXT, ask_mistral)
+
+    my_bot.add_handler(CommandHandler("start", start))
+    my_bot.add_handler(CommandHandler('mis', ask_mistral))
     my_bot.add_handler(chat_handler)
+    my_bot.add_handler(mistral_handler)
+
 
     my_bot.run_polling()
 
 
 async def start(update, context):
-    await update.message.reply_text('Я - бот, созданный на базе Cypher-Alpha AI. \n Напиши вопрос, чтобы ИИ ответил на него')
+    await update.message.reply_text('Я - бот, созданный на базе Cypher-Alpha AI.' 
+                                    '\n Напиши вопрос, чтобы ИИ ответил на него. По умолчанию установлен Cypher-Alpha AI. '
+                                    '\n Если хочешь воспользоваться Mistral Mini, напиши /mis перед запросом.' 
+                                    '\n\n\n Если возникли проблемы, сообщите @tarazz77.\n Вероятно, надо переподключить API key.')
 
 def chat(message):
     client = OpenAI(
@@ -34,10 +42,40 @@ def chat(message):
     )
     return completion.choices[0].message.content
 
+def mistral(image = '', request = ''):
+    client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=settings.MISTRAL_API,
+    )
+
+    completion = client.chat.completions.create(
+    model="mistralai/mistral-small-3.2-24b-instruct:free",
+    messages=[
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": request
+            }
+        ]
+        }
+    ]
+    )
+    return completion.choices[0].message.content
+
 async def ask(update, context):
     await update.message.reply_text('Пару секунд...')
     text = update.message.text
     await update.message.reply_text(chat(text))
+
+
+async def ask_mistral(update, context):
+    await update.message.reply_text('Собираю информацию...')
+    text = update.message.text
+    await update.message.reply_text(mistral(request=text))
+
+
 
 if __name__ == '__main__':
     main()
---
+++ mistral_config.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key="<OPENROUTER_API_KEY>",
+)
+
+completion = client.chat.completions.create(
+  model="mistralai/mistral-small-3.2-24b-instruct:free",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What is in this image?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+          }
+        }
+      ]
+    }
+  ]
+)
+print(completion.choices[0].message.content)
